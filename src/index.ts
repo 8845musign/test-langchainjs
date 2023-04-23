78ac7c75ca167c1e93024c82e6a439052ee28099
Hiroki Yokouchi <8845musign@gmail.com>
2023-04-23 17:25:30 +0900

    add prompt template

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
 
 require("dotenv").config();
 
 export const run = async () => {
   const llm = new OpenAI({ temperature: 0.9 });
 
-  const res = await llm.call(
-    "日本の首都は？"
-  );
+  const prompt = new PromptTemplate({
+    inputVariables: ["country"],
+    template: "{country}の首都は？"
+  })
+
+  const buildedPrompt = await prompt.format({ country: "アメリカ" });
+
+  const res = await llm.call(buildedPrompt);
   console.log(res);
 };
 run();
